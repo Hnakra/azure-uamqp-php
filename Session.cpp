fbc30a5e1d27a49e0e2c0999cad7d3da240ab375
Moroz Ivan <vanekmrz@yandex.ru>
2024-06-30 20:05:17 +0300

    Test session params

--- Session.cpp
+++ Session.cpp
@@ -7,8 +7,8 @@ Session::Session(Connection *connection)
     this->connection = connection;
 
     session = session_create(connection->getConnectionHandler(), NULL, NULL);
-    session_set_incoming_window(session, 2147483647);
-    session_set_outgoing_window(session, 65536);
+    //session_set_incoming_window(session, 2147483647);
+    //session_set_outgoing_window(session, 65536);
 }
 
 SESSION_HANDLE Session::getSessionHandler()
